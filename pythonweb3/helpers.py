bc31c4834277b9014b0f987e64455d807d4aec0c
Ali Parvizi <snparvizi75@gmail.com>
2022-04-10 21:37:02 -0700

    transfer tokens

--- pythonweb3/helpers.py
+++ pythonweb3/helpers.py
@@ -1,7 +1,13 @@
 import os
+
+from eth_typing import Address
+from web3.contract import Contract
+
 from erc20 import ERC20_ABI
 from setting import networks
 from web3 import Web3
+from web3.middleware import geth_poa_middleware
+
 
 def load_private_key() -> str:
     private_key = os.getenv("PRIVATE_KEY")
@@ -9,18 +15,21 @@ def load_private_key() -> str:
         raise Exception("[ERROR] Set PRIVATE_KEY env variable")
     return private_key
 
-def get_contract_address() -> str:
+
+def get_contract_address() -> Address:
     return networks['rinkeby']['erc20']
 
 
-def get_web3_object():
+def get_web3_object() -> Web3:
     w3 = Web3(Web3.HTTPProvider(os.getenv('RINKEBY_URL')))
+    w3.middleware_onion.inject(geth_poa_middleware, layer=0)
     if w3.isConnected():
         return w3
     else:
         raise Exception("Could not connect to provider ", os.getenv('RINKEBY_PROVIDER'))
 
-def get_erc20_contract():
+
+def get_erc20_contract() -> Contract:
     return get_web3_object().eth.contract(
-        address=get_contract_address(), 
+        address=get_contract_address(),
         abi=ERC20_ABI)
--- pythonweb3/main.py
+++ pythonweb3/main.py
@@ -3,17 +3,43 @@ from helpers import get_erc20_contract, get_web3_object, load_private_key, get_c
 
 load_dotenv()  # take environment variables from .env.
 
-def print_name_and_total_supply(erc20):
-    print("Token name: ", erc20.functions.name().call())
-    print("Total Supply: ", erc20.functions.totalSupply().call())
+w3 = get_web3_object()
+ERC20 = get_erc20_contract()
+PRIVATE_KEY = load_private_key()
+TO_ADDRESS = "0x13753419760c69c71f96C7732b2E1d24141c3Ab2"
+
+account = w3.eth.account.privateKeyToAccount(PRIVATE_KEY)
+
+
+def print_account_info():
+    print("[INFO] account address ", account.address)
+
+
+def print_name_and_total_supply():
+    print("[INFO] Token name: ", ERC20.functions.name().call())
+    print("[INFO] Total Supply: ", ERC20.functions.totalSupply().call())
+
+
+def transfer(amount):
+    print(f"[INFO] Transfer {amount} wei to {TO_ADDRESS}")
+    nonce = w3.eth.get_transaction_count(account.address)
+    print("[INFO] nonce: ", nonce)
+    send = ERC20.functions.transfer(TO_ADDRESS, amount)
+    tx = send.buildTransaction({
+        'from': account.address,
+        'nonce': nonce
+    })
+    signed_tx = w3.eth.account.sign_transaction(tx, PRIVATE_KEY)
+    tx_hash = w3.eth.send_raw_transaction(signed_tx.rawTransaction)
+    receipt = w3.eth.wait_for_transaction_receipt(tx_hash)
+    print("[INFO] receipt ", receipt)
+
 
 def run():
-    private_key = load_private_key()
-    contract = get_erc20_contract()
+    print_account_info()
+    print_name_and_total_supply()
+    transfer(10)
 
-    print_name_and_total_supply(contract)
-
-    
 
 if __name__ == "__main__":
     run()
