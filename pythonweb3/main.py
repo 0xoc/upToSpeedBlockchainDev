60dc452cb98445d97f370c2fd80fe76e5414c101
Ali Parvizi <snparvizi75@gmail.com>
2022-04-10 21:02:53 -0700

    print name and total supploy

--- pythonweb3/main.py
+++ pythonweb3/main.py
@@ -3,10 +3,17 @@ from helpers import get_erc20_contract, get_web3_object, load_private_key, get_c
 
 load_dotenv()  # take environment variables from .env.
 
+def print_name_and_total_supply(erc20):
+    print("Token name: ", erc20.functions.name().call())
+    print("Total Supply: ", erc20.functions.totalSupply().call())
+
 def run():
     private_key = load_private_key()
-    contract = get_erc20_contract()    
-    print(contract)
+    contract = get_erc20_contract()
+
+    print_name_and_total_supply(contract)
+
+    
 
 if __name__ == "__main__":
     run()
